f33bd22b70a639a67917ae35401f7618f98a6d28
Real96 <chicco.gaeta@gmail.com>
2020-06-29 06:11:11 +0200

    Removed hardcoded IP from CheckParty

--- tests/CheckParty.py
+++ tests/CheckParty.py
@@ -1,11 +1,13 @@
 # Go to root of PyNXBot
 import sys
+import  json
 sys.path.append('../')
 
 from structure import PK8
 from nxbot import SWSHBot
 
-b = SWSHBot('192.168.1.6')
+config = json.load(open("../config.json"))
+b = SWSHBot(config["IP"])
 for ii in range(1,7):
         pk8 = PK8(b.readParty(ii))
         print(pk8.toString())
