1c2b17571a314d268d329a187d03c4f68ba263e6
Real96 <chicco.gaeta@gmail.com>
2021-03-04 16:37:21 +0100

    Added some printings into Dumper.py

--- tests/Dumper.py
+++ tests/Dumper.py
@@ -9,11 +9,19 @@ config = json.load(open("../config.json"))
 
 b = SWSHBot(config["IP"])
 
-b.readEventBlock_RaidEncounter(DumpPath)
-b.readEventBlock_RaidEncounter_IoA(DumpPath)
-b.readEventBlock_RaidEncounter_CT(DumpPath)
-b.readEventBlock_DropRewards(DumpPath)
+print("Dumping bonus_rewards...")
 b.readEventBlock_BonusRewards(DumpPath)
+print("Dumping dai_encount...")
 b.readEventBlock_CrystalEncounter(DumpPath)
+print("Dumping drop_rewards...")
+b.readEventBlock_DropRewards(DumpPath)
+print("Dumping normal_encount...")
+b.readEventBlock_RaidEncounter(DumpPath)
+print("Dumping normal_encount_rigel1...")
+b.readEventBlock_RaidEncounter_IoA(DumpPath)
+print("Dumping normal_encount_rigel2...")
+b.readEventBlock_RaidEncounter_CT(DumpPath)
+
+print("\nDump completed!\n")
 
 b.close()
