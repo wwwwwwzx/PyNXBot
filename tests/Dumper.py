d08d1c226ac8c3b15cf33b00a7fc44ff2f277748
Real96 <chicco.gaeta@gmail.com>
2021-03-04 16:15:13 +0100

    Initial files dumper script
    Atm it only supports event den files dumping

---
+++ tests/Dumper.py
@@ -0,0 +1,19 @@
+# Go to root of PyNXBot
+import sys
+sys.path.append('../')
+from nxbot import SWSHBot
+import json
+
+DumpPath = 'Event/Current/'
+config = json.load(open("../config.json"))
+
+b = SWSHBot(config["IP"])
+
+b.readEventBlock_RaidEncounter(DumpPath)
+b.readEventBlock_RaidEncounter_IoA(DumpPath)
+b.readEventBlock_RaidEncounter_CT(DumpPath)
+b.readEventBlock_DropRewards(DumpPath)
+b.readEventBlock_BonusRewards(DumpPath)
+b.readEventBlock_CrystalEncounter(DumpPath)
+
+b.close()
