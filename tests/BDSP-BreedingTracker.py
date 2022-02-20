45bf9f33d4706b42b9e122230deecb6b115a09b7
zwab <51080317+zwab@users.noreply.github.com>
2022-02-20 00:36:46 +0000

    Fixed junk imports
    Accidentally added nonsensical imports during testing.

--- tests/BDSP-BreedingTracker.py
+++ tests/BDSP-BreedingTracker.py
@@ -30,6 +30,9 @@ eggSeed = egg.seed()
 eggSteps = egg.steps()
 print(f"Is egg ready? {isEggReady}\nEgg Seed: {eggSeed:08X}\nSteps for next egg: {180 - eggSteps}\n\n")
 
+dexOpened = False
+trainercardOpened = False
+scrolls = 0
 targetAdvances = 0
 botFlag = input("Press D-pad Down at a specific advance? (y/n) ")
 if botFlag == "y" or botFlag == "Y":
@@ -56,6 +59,49 @@ while True:
             print()
             print(f"Advances: {advances}\n")
             print(f"Is egg ready? {currIsEggReady}\nEgg Seed: {currEggSeed:08X}\nSteps for next egg: {180 - currEggSteps}\n\n")
+
+            if not dexOpened and botFlag and advances <= targetAdvances - 300:
+                print(f"Opening pokedex to advance...\n\n")
+                b.click("X")
+                b.pause(0.9)
+                b.click("A")
+                b.pause(0.9)
+                b.click("R")
+                b.pause(1.5)
+                dexOpened = True
+
+            if dexOpened and botFlag and advances <= targetAdvances - 300:
+                print(f"Pokedex scrolled {scrolls} times\n\n")
+                scrolls += 1
+                b.click("DRIGHT")
+                b.pause(0.2)
+
+            if dexOpened and botFlag and advances >= targetAdvances - 300:
+                print(f"Closing pokedex...\n\n")
+                b.click("B")
+                b.pause(0.9)
+                b.click("B")
+                b.pause(0.9)
+                dexOpened = False
+
+            if not trainercardOpened and botFlag and advances >= targetAdvances - 300 and advances <= targetAdvances - 30:
+                print(f"Opening trainercard to advance...\n\n")
+                b.click("X")
+                b.pause(0.9)
+                b.click("DLEFT")
+                b.pause(0.2)
+                b.click("A")
+                trainercardOpened = True
+
+            if trainercardOpened and botFlag and advances >= targetAdvances - 30:
+                print(f"Closing trainercard...\n\n")
+                b.click("B")
+                b.pause(0.9)
+                b.click("DRIGHT")
+                b.pause(0.2)
+                b.click("B")
+                b.pause(0.9)
+                trainercardOpened = False
 
             if botFlag and advances == targetAdvances:
                 for i in range(5):
